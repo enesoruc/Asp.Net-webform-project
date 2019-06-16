8cfd413ce89870bf9665e14130f50f23749f330d
enesoruc <orucenes95@gmail.com>
2019-06-16 16:15:39 +0300

    initial commit

---
+++ NWDAlistirma/NWD.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NWD.aspx.cs" Inherits="NWDAlistirma.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align:center;position:relative;width:100%;background-color:red;margin:auto">
+            <table style="text-align:center;position:relative;margin:auto">
+                <tr>
+                    <td>Kullanıcı Adı :</td>
+                    <td> <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox></td>
+                    <td> <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Kullanıcı Adı Alanı Boş Geçilemez" ForeColor="Red" ControlToValidate="txtUserName"></asp:RequiredFieldValidator></td>
+                </tr>
+                <tr>
+                    <td>Şifre :</td>
+                    <td><asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox></td>
+                    <td><asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Şifre Alanı Boş Geçilemez" ForeColor="Red" ControlToValidate="txtPassword"></asp:RequiredFieldValidator></td>
+                </tr>
+                <tr>
+                    <td><asp:Label ID="lblInfo" runat="server" Text=""></asp:Label></td>
+                    
+                    <td><asp:Button ID="btnSearch" runat="server" Text="Giriş" OnClick="btnSearch_Click" /></td>
+                </tr>
+            </table>
+            <div style="background-color:aqua">
+            <asp:DropDownList ID="ddlCustomers" runat="server" OnSelectedIndexChanged="ddlCutomers_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList><br />
+            <hr />
+            <asp:GridView ID="lstOrders" runat="server" AutoGenerateColumns="true"></asp:GridView>
+                </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ NWDAlistirma/NWD.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NWDAlistirma
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        NORTHWNDEntities db;
+        public WebForm1()
+        {
+            db = new NORTHWNDEntities();
+        }
+        string userName = string.Empty;
+        string password = string.Empty;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            userName = txtUserName.Text;
+            password = txtPassword.Text;
+        }
+
+        private void GetOrder(string customerID,string employee)
+        {
+            lstOrders.DataSource = db.Employees.Join(db.Orders,
+                                                      emp => emp.EmployeeID,
+                                                      o => o.EmployeeID,
+                                                      (emp, o) => new
+                                                      {
+                                                          MusteriID = o.CustomerID,
+                                                          calisan = emp.FirstName + "." + emp.LastName,
+                                                          siparisID = o.OrderID,
+                                                          siparisTarih = o.OrderDate,
+                                                      }).Join(db.Customers,
+                                                               empo => empo.MusteriID,
+                                                               c => c.CustomerID,
+                                                               (empo, c) => new
+                                                               {
+                                                                   MusteriID = empo.MusteriID,
+                                                                   calisanAd = empo.calisan,
+                                                                   siparisID = empo.siparisID,
+                                                                   siparisTarihi = empo.siparisTarih,
+                                                               }).Join(db.Order_Details,
+                                                                        empoc => empoc.siparisID,
+                                                                        od => od.OrderID,
+                                                                        (empoc, od) => new
+                                                                        {
+                                                                            musteriID = empoc.MusteriID,
+                                                                            urun = od.ProductID,
+                                                                            calisanAd = empoc.calisanAd,
+                                                                            siparisID = empoc.siparisID,
+                                                                            siparisTarih = empoc.siparisTarihi,
+                                                                        }).Join(db.Products,
+                                                                                empoc => empoc.urun,
+                                                                                p => p.ProductID,
+                                                                                (empoc, p) => new
+                                                                                {
+                                                                                    musteriID = empoc.musteriID,
+                                                                                    siparisNo = empoc.siparisID,
+                                                                                    siparisTarihi = empoc.siparisTarih,
+                                                                                    urunAdi = p.ProductName,
+                                                                                    calisanAd = empoc.calisanAd,
+                                                                                }).Where(a => a.calisanAd == userName && a.musteriID == customerID).Select(a => new { a.siparisNo, a.siparisTarihi, a.musteriID, a.urunAdi }).ToList();
+            lstOrders.DataBind();
+        }
+
+        
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            userName = txtUserName.Text;
+            string password = txtPassword.Text;
+            if (IsPostBack)//sayfa daha önceden yüklendiyse
+            {
+                userName = txtUserName.Text;
+                password = txtPassword.Text;
+            }
+            ddlCustomers.DataSource = db.Employees.Join(db.Orders,
+                                                      emp => emp.EmployeeID,
+                                                      o => o.EmployeeID,
+                                                      (emp, o) => new
+                                                      {
+                                                          MusteriID = o.CustomerID,
+                                                          UserName = emp.FirstName + "." + emp.LastName,
+                                                          Password=emp.LastName+"."+emp.BirthDate.Value.Year,
+
+                                                      }).Join(db.Customers,
+                                                               empo => empo.MusteriID,
+                                                               c => c.CustomerID,
+                                                               (empo, c) => new
+                                                               {
+                                                                   Musteri = c.CompanyName,
+                                                                   MusteriID=c.CustomerID,
+                                                                   UserName = empo.UserName,
+                                                                   Password=empo.Password,
+                                                               }).Where(a => a.UserName == userName && a.Password==password).Select(a =>new { a.Musteri,a.MusteriID }).ToList();
+            ddlCustomers.DataTextField = "Musteri"; // DisplayMember
+            ddlCustomers.DataValueField = "MusteriID"; // ValueMember
+            ddlCustomers.DataBind();
+            if (ddlCustomers.DataMember=="")
+            {
+                lblInfo.Text = "girilen bilgiler hatalı";
+                return;
+            }
+        }
+
+        protected void ddlCutomers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string customerID = ddlCustomers.SelectedValue;
+            GetOrder(customerID, userName);            
+        }
+    }
+}
